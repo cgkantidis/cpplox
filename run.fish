afe6317aa2e9fd9e7b0f7b4cda466b8747d910f2
Christos Gkantidis <cgkantid@proton.me>
2025-03-15 12:43:48 +0100

    Move the run script from sh to fish

--- run.fish
+++ run.fish
@@ -1,8 +1,11 @@
-if [ $# -eq 0 ]; then
-  BUILD_TYPE=Release
+#!/usr/bin/env fish
+
+if test (count $argv) -eq 0
+  set BUILD_TYPE "Release"
 else
-  BUILD_TYPE="$1"
-fi
+  set BUILD_TYPE "$argv[1]"
+end
+
 rm -rf build
 cmake -S . -B build -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=mold" && \
 cmake --build build -j$(nproc) && \
